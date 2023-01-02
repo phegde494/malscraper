0a3c9cb66a3cbd1e7a62f941a428f289e7679058
phegde494 <48624928+phegde494@users.noreply.github.com>
2023-01-01 20:38:49 -0500

    shortened a bit

--- malscraper.py
+++ malscraper.py
@@ -14,8 +14,7 @@ shows = soup.find_all('tr', class_="ranking-list")
 for show in shows:
     linkTag = show.find('td', class_="title al va-t word-break").find('div', class_="detail").find('div', class_="di-ib clearfix").find('h3', class_="hoverinfo_trigger fl-l fs14 fw-b anime_ranking_h3").a
     link = linkTag['href']
-    title = ""
-    for c in linkTag:
-        title += c
-    
+    title = linkTag.text
+    rating = show.find('td', class_="score ac fs14").div.find('span').text
+    print(rating)
     
